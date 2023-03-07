85dd66c4210bbf505e4dca9f637191cb000fee2b
Matt Mion <mmiondev@gmail.com>
2023-03-06 22:46:49 -0500

    create header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { scrollToSection, SectionNames } from "@/utils";
 import {
   Button,
   Divider,
@@ -5,41 +7,86 @@ import {
   Flex,
   useBreakpointValue,
   FlexProps,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
 } from "@chakra-ui/react";
-import React from "react";
-const sectionButtons = [
-  { name: "ABOUT" },
-  { name: "PROJECTS" },
-  { name: "CONTACT" },
-];
+import { HamburgerIcon } from "@chakra-ui/icons";
+
+const headerHeight = 46;
+const headerPaddingY = 8;
+export const heightMinusHeader = `calc(100vh - ${
+  headerHeight + headerPaddingY
+}px)`;
+
+const sectionButtons: SectionNames[] = ["about", "projects", "contact"];
+
+//TODO: button variants depending on what section is active, change bg to transparent afterwards
 
 export const Header = (props: FlexProps) => {
   const isMobile = useBreakpointValue({ base: true, md: false });
 
   return (
     <Flex
-      {...props}
-      py="2"
-      justify="center"
-      bg="transparent"
-      shadow="lg"
+      py={`${headerPaddingY}px`}
+      justify={{ base: "flex-end", sm: "center" }}
+      bgColor="primary.600"
+      shadow="xl"
+      px="10"
       position="sticky"
       top="0"
+      zIndex="banner"
+      {...props}
     >
-      <HStack spacing="1">
-        {sectionButtons.map((section, index) => (
-          <React.Fragment key={section.name}>
-            <Button size="sm">{section.name}</Button>
-            {index !== sectionButtons.length - 1 && (
-              <Divider
-                orientation="vertical"
-                height="46"
-                borderColor="secondary.500"
-              />
-            )}
-          </React.Fragment>
-        ))}
-      </HStack>
+      {isMobile ? (
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            aria-label="Menu"
+            icon={<HamburgerIcon color="secondary.500" boxSize="10" />}
+            variant="outline"
+          />
+          <MenuList bgColor="neutral.200">
+            {sectionButtons.map((section) => (
+              <MenuItem
+                as={Button}
+                key={section}
+                bg="transparent"
+                fontWeight="normal"
+                color="primary.600"
+                variant="iconPrimary"
+                onClick={() => scrollToSection(section)}
+              >
+                {section.toUpperCase()}
+              </MenuItem>
+            ))}
+          </MenuList>
+        </Menu>
+      ) : (
+        <HStack spacing="1">
+          {sectionButtons.map((section, index) => (
+            <React.Fragment key={section}>
+              <Button
+                size="sm"
+                fontWeight="normal"
+                variant="iconSecondary"
+                onClick={() => scrollToSection(section)}
+              >
+                {section.toUpperCase()}
+              </Button>
+              {index !== sectionButtons.length - 1 && (
+                <Divider
+                  orientation="vertical"
+                  height={`${headerHeight}px`}
+                  borderColor="secondary.500"
+                />
+              )}
+            </React.Fragment>
+          ))}
+        </HStack>
+      )}
     </Flex>
   );
 };
